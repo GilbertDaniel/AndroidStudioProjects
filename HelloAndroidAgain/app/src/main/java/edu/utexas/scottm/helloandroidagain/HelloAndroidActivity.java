958a9a8bb2e50bffaa4d2d8f69751bb22ce5cda6
scott-u-texas <scottm@cs.utexas.edu>
2015-01-26 15:46:04 -0600

    Initial Repo
    Creation of Repo for for CS371m Coding Examples.
    
    University of Texas at Austin
    CS371m - Mobile Computing - Android App Development
    
    Educational Examples of various Android apps.

---
+++ HelloAndroidAgain/app/src/main/java/edu/utexas/scottm/helloandroidagain/HelloAndroidActivity.java
@@ -0,0 +1,19 @@
+package edu.utexas.scottm.helloandroidagain;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class HelloAndroidActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_android);
+    }
+
+
+
+}
